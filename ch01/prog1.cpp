13c138e0b99f74210da38c61c0d5aa4f832d773d
xqk <xu_qinkun@163.com>
2026-08-06 22:29:47 +0800

    code runner

--- ch01/prog1.cpp
+++ ch01/prog1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Enter two numbers:" << std::endl;
+    std::cout << "Enter two numbers:\n";
     int v1 = 0, v2 = 0;
     (std::cin >> v1) >> v2;
-    std::cout << "The 1sum of " << v1 << " and " << v2
-              << " is " << v1 + v2 << std::endl;
+    std::cout << "The sum of " << v1;
+    std::cout << " and " << v2;
+    std::cout << " is = " << v1 + v2 << std::endl;
     return 0;
 }
